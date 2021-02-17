c81d12fc4f95e624a9cb6e21e7ab125dfdc0ebdc
DESKTOP-7JNUVEL\Admin <anpham4474@gmail.com>
2021-02-17 14:12:00 +0700

    Remove animation.js file

--- animation.js
+++
@@ -1,9 +0,0 @@
-const banner = document.querySelector('.banner');
-
-setInterval(() => {
-    banner.style.color = '#8c33ff';
-}, 1300);
-
-setInterval(() => {
-    banner.style.color = 'white';
-}, 2600);
